44e305bd5f30cc66d375243060a82cdc95353d8e
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-04-13 11:18:45 -0400

    Initial Commit

---
+++ AVFoundation-MediaFeed/MediaFeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AVFoundation-MediaFeed
+//
+//  Created by Christian Hurtado on 4/13/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class MediaFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
